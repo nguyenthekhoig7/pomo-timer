508edaf79116f66f871fceee1120a974c9ad6b26
nguyenthekhoig7 <nguyenthekhoig7@gmail.com>
2023-06-20 11:50:55 +0700

    feat: simplify streamlit

--- pomo_streamlit.py
+++ pomo_streamlit.py
@@ -4,25 +4,25 @@ import time
 
 def count_down(time_amount, message_placeholder):
     ss = (time_amount.hour * 60 + time_amount.minute) * 60 + time_amount.second
-    stop_bttn = st.button('Stop')
-    reset_bttn = st.button('Reset')
+    # stop_bttn = st.button('Stop')
+    # reset_bttn = st.button('Reset')
     while ss > 0:
         message_placeholder.write(f"Time left: {ss}s")
         time.sleep(1)
         ss -= 1
 
-        if stop_bttn:
-            message_placeholder.write(f"Time left: {ss}s")
-            break
-        if reset_bttn:
-            stop_count_down()
-            break
+        # if stop_bttn:
+        #     message_placeholder.write(f"Time left: {ss}s")
+        #     break
+        # if reset_bttn:
+        #     stop_count_down()
+        #     break
 
-def stop_count_down():
-    st.write("Input time then click 'Start!'")
+# def stop_count_down():
+#     st.write("Input time then click 'Start!'")
 
 st.title('Pomodoro timer')
-time_input = st.time_input('Set a timer for', datetime.time(0, 10))
+time_input = st.time_input('Set a timer for', datetime.time(0, 1))
 start_bttn = st.button('Start!')
 message_placeholder = st.empty()
 if start_bttn:
